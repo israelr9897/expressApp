a7c0db69e36048255cd26cbf2a00cdb8bcca7a3a
israelr9897 <israelr9897@gmail.com>
2025-07-08 09:37:10 +0300

    instalss to modols

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 import express from "express";
 
+const PORT = 3050;
+const APP = express();
+
+APP.get("/", (req, res) => {
+  res.end("wef");
+});
+
+APP.listen(PORT, () => {
+  console.log("express server runing on port:" + PORT);
+});
